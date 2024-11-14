286345ddb30a34066c11bfdd49d9cccadff4837e
B CHUCHEL ASHISH <85539405+ashishrockz@users.noreply.github.com>
2024-11-15 01:48:04 +0530

    a
    a

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const connection = require('./service/db');
-const authentication = require('./controllers/authController');
+const authentication = require('./controllers/authController'); // Fix route path if incorrect
 const posts = require('./routers/postRoutes');
 const verifyToken = require('./middleware/auth');
 const { addComment } = require('./controllers/commentController');
@@ -29,24 +29,24 @@ app.post('/comments', verifyToken, addComment);
 // Like route
 app.post('/likes', verifyToken, toggleLike);
 
-// const __dirname1 =path.resolve();
-// if(process.env.NODE_ENV === "production"){
-//   app.use(express.static(path.join(__dirname1, "/client/build/")));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname1, "client", "bulid" , "index.html"));
-//   });
-// }else{
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+} else {
   app.get('/', (req, res) => {
     res.send('Welcome to the API');
   });
-// }
+}
 
 // Catch-all route for undefined routes
 app.use((req, res) => {
   res.status(404).send('404: Not Found');
 });
 
-const port = process.env.PORT || 8080; // Default to port 8080 if not specified in .env
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server running on port ${port}...`);
 });
